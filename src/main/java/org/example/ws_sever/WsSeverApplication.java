e29116d989f936e4aa3631ee396ee24b62bcae81
pleaserun <nabeiyq@126.com>
2025-03-02 10:32:07 +0800

    test

---
+++ src/main/java/org/example/ws_sever/WsSeverApplication.java
@@ -0,0 +1,13 @@
+package org.example.ws_sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsSeverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ws_sever/WsSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ws_sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
